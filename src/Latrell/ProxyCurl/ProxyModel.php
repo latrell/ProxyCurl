3c5387b503fc6fc396f72211c2eb49eb78fd6bd0
Latrell Chan <i@latrell.me>
2019-08-21 14:56:20 +0800

    no message

--- src/Latrell/ProxyCurl/ProxyModel.php
+++ src/Latrell/ProxyCurl/ProxyModel.php
@@ -2,7 +2,10 @@
 
 namespace Latrell\ProxyCurl;
 
-class ProxyModel
+use ArrayAccess;
+use JsonSerializable;
+
+class ProxyModel implements ArrayAccess, JsonSerializable
 {
 	public $ip;
 	public $port;
@@ -14,13 +17,110 @@ class ProxyModel
 
 	public function __toString()
 	{
-		return json_encode([
+		return $this->toJson();
+	}
+
+	public function toJson()
+	{
+		return json_encode($this->toArray(), JSON_UNESCAPED_UNICODE);
+	}
+
+	public function toArray()
+	{
+		return [
 			'ip' => $this->ip,
 			'port' => $this->port,
 			'address' => $this->address,
 			'isp' => $this->isp,
 			'export_ip' => $this->export_ip,
 			'timeout' => (string) $this->timeout,
-		], JSON_UNESCAPED_UNICODE);
+		];
+	}
+
+	/**
+	 * Whether a offset exists
+	 *
+	 * @link https://php.net/manual/en/arrayaccess.offsetexists.php
+	 *
+	 * @param mixed $offset <p>
+	 * An offset to check for.
+	 * </p>
+	 *
+	 * @return boolean true on success or false on failure.
+	 * </p>
+	 * <p>
+	 * The return value will be casted to boolean if non-boolean was returned.
+	 * @since 5.0.0
+	 */
+	public function offsetExists($offset)
+	{
+		return key_exists($offset, $this->toArray());
+	}
+
+	/**
+	 * Offset to retrieve
+	 *
+	 * @link https://php.net/manual/en/arrayaccess.offsetget.php
+	 *
+	 * @param mixed $offset <p>
+	 * The offset to retrieve.
+	 * </p>
+	 *
+	 * @return mixed Can return all value types.
+	 * @since 5.0.0
+	 */
+	public function offsetGet($offset)
+	{
+		return $this->$offset;
+	}
+
+	/**
+	 * Offset to set
+	 *
+	 * @link https://php.net/manual/en/arrayaccess.offsetset.php
+	 *
+	 * @param mixed $offset <p>
+	 * The offset to assign the value to.
+	 * </p>
+	 * @param mixed $value <p>
+	 * The value to set.
+	 * </p>
+	 *
+	 * @return void
+	 * @since 5.0.0
+	 */
+	public function offsetSet($offset, $value)
+	{
+		$this->$offset = $value;
+	}
+
+	/**
+	 * Offset to unset
+	 *
+	 * @link https://php.net/manual/en/arrayaccess.offsetunset.php
+	 *
+	 * @param mixed $offset <p>
+	 * The offset to unset.
+	 * </p>
+	 *
+	 * @return void
+	 * @since 5.0.0
+	 */
+	public function offsetUnset($offset)
+	{
+		$this->$offset = null;
+	}
+
+	/**
+	 * Specify data which should be serialized to JSON
+	 *
+	 * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+	 * @return mixed data which can be serialized by <b>json_encode</b>,
+	 * which is a value of any type other than a resource.
+	 * @since 5.4.0
+	 */
+	public function jsonSerialize()
+	{
+		return $this->toArray();
 	}
 }
